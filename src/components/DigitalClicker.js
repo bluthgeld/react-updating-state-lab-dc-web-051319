0d8cc06c4d1252e73caa9c31f0cec37210599447
Robert Pedersen <rob@worldcrimeleague.org>
2019-07-17 13:48:47 -0400

    done

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,32 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+class DigitalClicker extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      timesClicked: 0
+    };
+  }
+
+  clickMe = () => {
+    this.setState({
+      timesClicked: this.state.timesClicked + 1
+    })
+    console.log(this.state.timesClicked)
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.clickMe}>{this.state.timesClicked}</button>
+      </div>
+    );
+  }
+
+}
+
+export default DigitalClicker
